262f798a12c227b8a1bbdfc406346cd57ddf588a
shirishstha <sthasiriz123@gmail.com>
2025-07-23 01:05:10 +0545

    added spinner in admin login

--- client/src/pages/admin/login.jsx
+++ client/src/pages/admin/login.jsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { Spinner } from '@/components/ui/spinner'
 import axios from 'axios'
 import React, { useState } from 'react'
 import toast from 'react-hot-toast'
@@ -10,32 +11,40 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
 
     const handleSubmit = async () => {
+        setLoading(true);
         //validations
-        if(!email || !password){
+        if (!email || !password) {
             return toast.error("Both creditials must be filled");
         }
         const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-        if(!emailRegex.test(email)){
+        if (!emailRegex.test(email)) {
             return toast.error("Invalid email");
         }
 
         //sent data to backend
-        const res = await axios.post(`${import.meta.env.VITE_BACKENDAPI}/adminLogin`,{email, password});
+        const res = await axios.post(`${import.meta.env.VITE_BACKENDAPI}/adminLogin`, { email, password });
+        setLoading(false);
 
-        if(!res.data.success){
+        if (!res.data.success) {
             return toast.error(res.data.message);
         }
 
         toast.success(res.data.message);
-        localStorage.setItem('admin',res.data.success);
+        localStorage.setItem('admin', res.data.success);
         navigate('/admin/codewar');
 
     }
 
     return (
         <div className='w-full flex justify-center h-screen items-center'>
+            {loading && (
+                <div className="fixed top-0 left-0 w-full h-full bg-black/20 backdrop-blur-sm z-[1000] flex items-center justify-center">
+                    <Spinner />
+                </div>
+            )}
             <div className='shadow-[0_0_5px]  p-5 w-80 md:w-100 lg:w-120 px-10 space-y-5 rounded-lg m-5'>
                 <h1 className='text-2xl font-medium gradientEffect'>Admin Login </h1>
                 <div className='grid gap-2'>
@@ -59,7 +68,7 @@ const Login = () => {
                 </div>
 
 
-                <Button onClick = {() => handleSubmit()} >Login</Button>
+                <Button onClick={() => handleSubmit()} >Login</Button>
             </div>
         </div>
 
